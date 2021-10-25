3a6efc012c5ab165d728a2cf003e178f6586e84e
lesleu <leselleleuterio5@gmail.com>
2021-10-25 10:44:07 -0700

    Added Pages
    Full layout, added all pages.
    
    Tried to add a navigation that activated at certain section of page

--- js/index.js
+++ js/index.js
@@ -14,6 +14,13 @@ $ (".topNavRight").hover (function(){
     $(this).removeClass("topNavHover");
 });
 
+$ (".projectBox").hover (function(){
+    $(this).addClass("projectHover");
+}, function(){
+    $(this).removeClass("projectHover");
+});
+
+
 const scrollOffset = 300;
 
 const scrollElements = document.querySelector(".textScroll");
@@ -46,3 +53,23 @@ const handleScrollAnimation = () => {
 window.addEventListener('scroll', () => {
     handleScrollAnimation();
 })
+
+$(document).ready(function() {
+    //change the integers below to match the height of your upper div, which I called
+    //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+    //to figure out what the scroll position is when exactly you want to fix the nav
+    //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+    //you know the position.
+    $(window).scroll(function () { 
+  
+      console.log($(window).scrollTop());
+  
+      if ($(window).scrollTop() > 1000) {
+        $('.projectNav').addClass('navbarFixed');
+      }
+  
+      if ($(window).scrollTop() < 1001) {
+        $('.projectNav').removeClass('navbarFixed');
+      }
+    });
+  });
